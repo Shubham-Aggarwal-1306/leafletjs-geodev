4d13dd33f3b57c27b40cf045f52a16a21b5fe085
Shubham Aggarwal <aggarwalshubham026@gmail.com>
2024-04-28 14:49:31 +0530

    first commit

---
+++ data/line.js
@@ -0,0 +1,131 @@
+const lineJson =
+{
+    "type": "FeatureCollection",
+    "features": [
+      {
+        "type": "Feature",
+        "properties": {},
+        "geometry": {
+          "coordinates": [
+            89.5351233676297,
+            24.83996506458645
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {},
+        "geometry": {
+          "coordinates": [
+            88.5953955153916,
+            24.74763754424741
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {},
+        "geometry": {
+          "coordinates": [
+            89.75886809435315,
+            24.448025936749502
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {},
+        "geometry": {
+          "coordinates": [
+            89.06729348448062,
+            24.455432377976976
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {},
+        "geometry": {
+          "coordinates": [
+            89.72632842579748,
+            23.850385752293093
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {},
+        "geometry": {
+          "coordinates": [
+            90.56434826223045,
+            25.00966723098115
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {},
+        "geometry": {
+          "coordinates": [
+            87.83066496005677,
+            24.496140305977306
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {},
+        "geometry": {
+          "coordinates": [
+            88.94118282032736,
+            25.36675016550757
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {},
+        "geometry": {
+          "coordinates": [
+            [
+              89.25849352367987,
+              24.729163798375694
+            ],
+            [
+              89.37646801595236,
+              24.03257338831186
+            ],
+            [
+              88.77845938271014,
+              24.325756884475894
+            ],
+            [
+              88.73777852330636,
+              24.799349423112076
+            ],
+            [
+              89.25442543774005,
+              24.725468719903617
+            ],
+            [
+              88.98186367973108,
+              24.566476694604532
+            ],
+            [
+              89.63275743019983,
+              25.031785646630183
+            ]
+          ],
+          "type": "LineString"
+        }
+      }
+    ]
+  }
